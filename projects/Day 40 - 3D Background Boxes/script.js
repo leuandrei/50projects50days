630772c16dea4e03fdb9dff9ca44c894ba4d1613
leuandrei <leuandrei01@gmail.com>
2025-03-05 01:06:36 +0200

    Added Day 40 Project Files

---
+++ projects/Day 40 - 3D Background Boxes/script.js
@@ -0,0 +1,19 @@
+const boxes = document.getElementById('boxes');
+const btn = document.getElementById('btn');
+
+function createBoxes() {
+    for (let i = 0; i < 4; i++){
+        for (let j = 0; j < 4; j++){
+            const box = document.createElement('div');
+            box.classList.add('box');
+            box.style.backgroundPosition = `-${125 * j}px -${125 * i}px`;
+            boxes.appendChild(box);
+        }
+    }
+}
+
+createBoxes();
+
+btn.addEventListener('click', () => {
+    boxes.classList.toggle('big');
+})
